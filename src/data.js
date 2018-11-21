5c8a4c3133761b77517d5374d01b530bfd5bd6d6
Johanna <J.Kraken24@hotmail.com>
2018-11-21 11:58:40 +0100

    edit data

--- src/data.js
+++ src/data.js
@@ -1,16 +1,83 @@
 
 // var exports = module.exports = {};
 
-exports.tags = [
-    {id: 0, name: "general"}, {id: 1, name: "Helsinki"}, {id: 2, name: "development"}, 
-    {id: 3, name: "managment"}, {id: 4, name: "Helsafter workinki"}, {id: 5, name: "marketing"}, 
-    {id: 6, name: "training"}, {id: 7, name: "meeting"}
-];
+var Tag = class Tag {
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+}
+var Guidline = class Guidline {
+    constructor(id, name, text){
+        this.id = id;
+        this.name = name;
+        this.text = text;
+    }
+}
+var Event = class Event {
+    constructor(id, title, des, date){
+        this.id = id;
+        this.title = title;
+        this.description = des;
+        this.date = date;
+        this.start_time = "";
+        this.end_time = "";
+        this.noti_date = new Date();
+        this.not_id = 0;
+    }
+}
+var User = class User {
+    constructor(id, slack_user_id, name){
+        this.id = id;
+        this.slack_user_id = slack_user_id;
+        this.name = name;
+    }
+}
+var Notification = class Notification{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+}
+var eventTag = class EventTag{
+    constructor(id, eid, tid){
+        this.id = id;
+        this.event_id = eid;
+        this.tag_id = tid;
+    }
+}
+var userEvent = class UserEvent{
+    constructor(id, eid, uid){
+        this.id = id;
+        this.event_id = eid;
+        this.user_id = uid;
+        this.noti_date = new Date();
+        this.noti_id = 0;
+    }
+}
 
-exports.guidlines = [
-    {id: 0, name: "language", text: "For a better understanding of non-Finnish speakers, the English language should be preferred."}, 
-    {id: 1, name: "tags", text: "To have a better conection between slack and confuence we decided to use the same tags in both. \nTo see the tags plese use <@UE743CUJZ> tags"}
-];
+var tagArray = [];
+tagArray.push(new Tag(0, "general"));tagArray.push(new Tag(1, "Helsinki"));tagArray.push(new Tag(2, "development"));tagArray.push(new Tag(3, "managment"));tagArray.push(new Tag(4, "Helsafter workinki"));
+tagArray.push(new Tag(5,"marketing"));tagArray.push(new Tag(6, "training"));tagArray.push(new Tag(7, "meeting"));tagArray.push();tagArray.push();
+exports.tags = tagArray;
+
+var guidlineArray = [];
+guidlineArray.push(new Guidline(0, "language", "For a better understanding of non-Finnish speakers, the English language should be preferred."));
+guidlineArray.push(new Guidline(1, "tags", "To have a better conection between slack and confuence we decided to use the same tags in both. \nTo see the tags plese use <@UE743CUJZ> tags"));
+exports.guidlines = guidlineArray;
+
+var userArray = [];
+exports.user = userArray;
+
+var notiArray = [];
+notiArray.push(new Notification(0, "morning"));notiArray.push(new Notification(1, "day before"));notiArray.push(new Notification(2, "week before"));
+exports.noti = notiArray;
+
+var eventTagArray = [];
+exports.eventTag = eventTagArray;
+
+var userEventArray = [];
+exports.userEvent = userEventArray;
 
     //button
     /*{
